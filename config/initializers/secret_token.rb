0f9e87a4fed284e73a8f0c49cd027cc9666f5291
heri16 <indo@singapore.com>
2010-12-18 17:12:50 +0900

    first rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blackbelt::Application.config.secret_token = '299779f21c52f2349333a9a3ac9a5049e28561dd435fbd02dffc5cad92cdf35c097b2e25275d1d9155f68d6aa8bfdda02ea6f567573b527bac58ec3878dfc5bd'
